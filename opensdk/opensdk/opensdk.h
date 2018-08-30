8ad06e531e12213cca7503cfa7ae687d562e7148
moubuns <mengwl@fox.one>
2018-08-30 21:03:23 +0800

    init project

---
+++ opensdk/opensdk/opensdk.h
@@ -0,0 +1,19 @@
+//
+//  opensdk.h
+//  opensdk
+//
+//  Created by moubuns on 2018/8/30.
+//  Copyright © 2018 FoxOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for opensdk.
+FOUNDATION_EXPORT double opensdkVersionNumber;
+
+//! Project version string for opensdk.
+FOUNDATION_EXPORT const unsigned char opensdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <opensdk/PublicHeader.h>
+
+
