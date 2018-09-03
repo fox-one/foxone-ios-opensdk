7399fe18fedb8781e9f95b9100e1289d989fcde6
moubuns <mengwl@fox.one>
2018-09-03 10:39:34 +0800

    add install demo

---
+++ opensdk/SDKDemo/AppDelegate.swift
@@ -0,0 +1,47 @@
+//
+//  AppDelegate.swift
+//  SDKDemo
+//
+//  Created by moubuns on 2018/9/3.
+//  Copyright © 2018 FoxOne. All rights reserved.
+//
+
+import UIKit
+import FoxOneOpenSDK
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        OpenSDK.setDelegate(self)
+        return true
+    }
+}
+
+extension AppDelegate: OpenSDKProtcol {
+    func f1AccessToken() -> String {
+        return ""
+    }
+    
+    func f1PublicKey() -> String {
+        return ""
+    }
+    
+    func f1HostURLString() -> String {
+        return "baseURL"
+    }
+    
+    func f1HttpHeader() -> [String: String] {
+       
+        
+        return ["x-client-build": "buildVersion",
+                "x-client-type": "clientType",
+                "x-client-version": "appVersion",
+                "x-client-device-id": "uuid"]
+    }
+    
+}
+
---
+++ opensdk/SDKDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SDKDemo
+//
+//  Created by moubuns on 2018/9/3.
+//  Copyright © 2018 FoxOne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
